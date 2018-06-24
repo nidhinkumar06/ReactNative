0bf189e87a8e90bec11483a967485af8b9286c19
nidhinkumar <nidhinkumar@codingtown.com>
2018-06-24 21:27:07 +0530

    Create notes taker

---
+++ NotesTaker/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Main from './src/components/Main';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
