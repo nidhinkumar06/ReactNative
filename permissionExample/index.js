40e5ff3c0ca0237fbd03cd02384f1b5a1809efa7
nidhinkumar <nidhinkumar@codingtown.com>
2018-08-20 21:22:48 +0530

    Add taskman App

---
+++ permissionExample/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import PermissionExample from './src/permissionExample';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => PermissionExample);
